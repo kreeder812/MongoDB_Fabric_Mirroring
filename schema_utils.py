577bc98b69dafe75b59e9450dfcf834f72b7c827
kreeder812 <48503207+kreeder812@users.noreply.github.com>
2026-07-22 14:59:24 -0700

    Refactor dtype conversion logic in schema_utils.py

--- schema_utils.py
+++ schema_utils.py
@@ -410,8 +410,8 @@ def process_dataframe(table_name_param: str, df: pd.DataFrame):
         ##if current_dtype == datetime and schema_of_this_column[DTYPE_KEY] == object:
         #print(f"****is_datetime64_any_dtype(df['col_name']): {is_datetime64_any_dtype(df[col_name])}")
         #print(f"****is_object_dtype(schema_of_this_column[DTYPE_KEY]): {is_object_dtype(schema_of_this_column[DTYPE_KEY])}")
-
-        if is_datetime64_any_dtype(df[col_name]) and is_object_dtype(schema_of_this_column[DTYPE_KEY]):
+###
+    if is_datetime64_any_dtype(df[col_name]) and is_object_dtype(schema_of_this_column[DTYPE_KEY]):
             df[col_name] = df[col_name].astype(str)
         elif current_dtype.__str__() != schema_of_this_column[DTYPE_KEY].__str__():
             try:
@@ -430,6 +430,8 @@ def process_dataframe(table_name_param: str, df: pd.DataFrame):
                     ).astype(target_dtype)
                 elif pd.api.types.is_numeric_dtype(target_dtype):
                     df[col_name] = pd.to_numeric(df[col_name], errors='coerce').astype(target_dtype)
+                elif is_object_dtype(target_dtype):
+                    df[col_name] = df[col_name].astype(str)
                 else:
                     df[col_name] = df[col_name].astype(target_dtype)
 
@@ -439,7 +441,7 @@ def process_dataframe(table_name_param: str, df: pd.DataFrame):
                     + f"the dtype of the column {col_name} from {current_dtype} to {schema_of_this_column[DTYPE_KEY]}"
                 )
 
-
+###
     
     # Check if conversion log file exists before pushing
     print("conversion_flag: ", conversion_flag)
